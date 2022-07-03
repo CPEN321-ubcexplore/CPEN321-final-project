e676f8094731ee6befa9f643225340439cc0a684
dpither <dylanpither@gmail.com>
2022-07-03 14:07:58 -0700

    More functions setdisplayname, sending friend requests

--- backend/users/users_server.js
+++ backend/users/users_server.js
@@ -1,5 +1,6 @@
 var mysql = require('mysql');
 var express = require('express');
+const { response } = require('express');
 var app = express();
 
 const Difficulty = {
@@ -35,7 +36,7 @@ async function run() {
 			console.log("Using usersdb.");
 		});
 		account = await login(0, "Dylan");
-		account.changeDifficulty("Medium");
+		console.log(account.friends);
 	}
 	catch (err) {
 		console.log(err);
@@ -48,12 +49,17 @@ async function find(displyName) {
 	return new Promise((resolve, reject) => {
 		con.query(sql, async function (err, result) {
 			if (err) reject(err);
-			account = new UserAccount(result[0].displayName, result[0].score, result[0].difficulty, result[0].leaderboardParticipant, result[0].user_id);
-			await account.getFriends();
-			await account.getIncomingRequests();
-			await account.getOutgoingRequests();
-			//Need to also add collection and locations
-			resolve(account);
+			else if(result[0] == undefined){
+				resolve(result[0]);
+			}
+			else{
+				account = new UserAccount(result[0].displayName, result[0].score, result[0].difficulty, result[0].leaderboardParticipant, result[0].user_id);
+				await account.getFriends();
+				await account.getIncomingRequests();
+				await account.getOutgoingRequests();
+				//Need to also add collection and locations
+				resolve(account);
+			}
 		})
 	});
 }
@@ -102,34 +108,21 @@ class UserAccount {
 	getDifficulty() {
 		return this.difficulty;
 	}
-	changeDifficulty(difficulty){
+	changeDifficulty(difficulty) {
 		validDifficulty(difficulty);
-		if(this.difficulty != difficulty){
+		if (this.difficulty != difficulty) {
 			this.difficulty = difficulty;
 			var sql = `UPDATE useraccounts SET difficulty = '${difficulty}' WHERE user_id = '${this.id}'`;
-			con.query(sql, function(err, result){
-				if(err) throw err;
-				console.log("Changed difficulty to: " + difficulty);
+			con.query(sql, function (err, result) {
+				if (err) throw err;
 			})
 		};
-	}
-	async sendRequest(displayName) {
-		var receiver = await find(displayName);
-		if (receiver == undefined) {
-			throw new Error("Account " + displayName + " does not exist");
-		}
-		else {
-			var receiver_id = receiver.user_id;
-			var send_id = this.id;
-			var status = Status.Pending;
-			var sql = `INSERT INTO friendships(send_id,receiver_id,status) VALUES ('${send_id}','${receiver_id}','${status}')`;
-		}
 	}
 	async getFriends() {
 		var account = this;
 		var friends = [];
 		var sql =
-		`SELECT displayName
+			`SELECT displayName
 		FROM useraccounts
 		INNER JOIN friendships ON friendships.send_id = useraccounts.user_id OR friendships.receiver_id = useraccounts.user_id
 		WHERE user_id != '${this.id}' AND status = '${Status.Friends}' AND (receiver_id = '${this.id}' OR send_id = '${this.id}')`;
@@ -148,13 +141,13 @@ class UserAccount {
 		var account = this;
 		var incomingRequests = [];
 		var sql =
-		`SELECT displayName
+			`SELECT displayName
 		FROM useraccounts
 		JOIN friendships ON friendships.send_id = useraccounts.user_id OR friendships.receiver_id = useraccounts.user_id
 		WHERE user_id != '${this.id}' AND status = '${Status.Pending}' AND receiver_id = '${this.id}'`;
 		return new Promise((resolve, reject) => {
 			con.query(sql, function (err, result) {
-				if(err) reject(err);
+				if (err) reject(err);
 				for (var i = 0; i < result.length; i++) {
 					incomingRequests.push(result[i].displayName);
 				}
@@ -166,14 +159,14 @@ class UserAccount {
 	async getOutgoingRequests() {
 		var account = this;
 		var outgoingRequests = [];
-		var sql = 
-		`SELECT displayName
+		var sql =
+			`SELECT displayName
 		FROM useraccounts
 		JOIN friendships ON friendships.send_id = useraccounts.user_id OR friendships.receiver_id = useraccounts.user_id
 		WHERE user_id != '${this.id}' AND status = '${Status.Pending}' AND send_id = '${this.id}'`;
 		return new Promise((resolve, reject) => {
 			con.query(sql, function (err, result) {
-				if(err) reject(err);
+				if (err) reject(err);
 				for (var i = 0; i < result.length; i++) {
 					outgoingRequests.push(result[i].displayName);
 				}
@@ -182,12 +175,73 @@ class UserAccount {
 			})
 		});
 	}
+	async sendRequest(displayName) {
+		var receiver = await find(displayName);
+		var receiver_id = receiver.id;
+		var send_id = this.id;
+		if (receiver == undefined) {
+			throw new Error("Account " + displayName + " does not exist");
+		}
+		var err = await friendshipExists(send_id, receiver_id)
+		//Dont send request if friendship already exists/is pending
+		if (err instanceof Error) {
+			throw err;
+		}
+		else {
+			var status = Status.Pending;
+			var sql = `INSERT INTO friendships(send_id,receiver_id,status) VALUES ('${send_id}','${receiver_id}','${status}')`;
+			return new Promise((resolve, reject) => {
+				con.query(sql, function (err, result) {
+					if (err) reject(err);
+					console.log("User " + send_id + " sent a friend request to " + receiver_id);
+					resolve(result);
+				})
+			})
+		}
+	}
+	async setDisplayName(displayName) {
+		var account = await find(displayName);
+		//Name taken
+		if(account != undefined){
+			throw new Error ("Name taken");
+		}
+		else{
+			var sql = `UPDATE useraccounts SET displayName = '${displayName}' WHERE user_id = '${this.id}'`;
+			con.query(sql, function (err, result) {
+				if (err) throw err;
+			});
+		}
+	}
 }
 
-function validDifficulty(difficulty){
-	if(difficulty != Difficulty.Easy && difficulty != Difficulty.Medium){
+function validDifficulty(difficulty) {
+	if (difficulty != Difficulty.Easy && difficulty != Difficulty.Medium) {
 		throw new Error("Invalid difficulty");
 	}
+}
+
+async function friendshipExists(send_id, receiver_id) {
+	var sql =
+		`SELECT * 
+	FROM friendships 
+	WHERE (send_id = '${send_id}' AND receiver_id = '${receiver_id}') OR (send_id = '${receiver_id}' AND receiver_id = '${send_id}')`;
+	return new Promise((resolve, reject) => {
+		con.query(sql, function (err, result) {
+			if (err) reject(err);
+			else if (result[0] == undefined) {
+				resolve();
+			}
+			else if (result[0].status == Status.Friends) {
+				reject(new Error("You are already friends with this user"));
+			}
+			else if (result[0].send_id == send_id) {
+				reject(new Error("You have already sent a request to this user"));
+			}
+			else if (result[0].receiver_id == send_id) {
+				reject(new Error("You have been sent a request by this user"));
+			}
+		})
+	})
 }
 
 run()
