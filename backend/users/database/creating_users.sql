547a0cecf2093b1ecfbcebccbe65023ba158ee98
dpither <dylanpither@gmail.com>
2022-07-04 00:44:43 -0700

    Changed database setup to work with login ids

--- backend/users/database/creating_users.sql
+++ backend/users/database/creating_users.sql
@@ -1,33 +1,32 @@
 CREATE DATABASE usersdb;
 USE usersdb;
 CREATE TABLE collections (
-  user_id int NOT NULL,
+  user_id varchar(255) NOT NULL,
   itemName varchar(45) NOT NULL,
   PRIMARY KEY (user_id,itemName)
 );
 
 CREATE TABLE friendships (
   friendship_id int NOT NULL AUTO_INCREMENT,
-  send_id int NOT NULL,
-  receiver_id int NOT NULL,
+  send_id varchar(255) NOT NULL,
+  receiver_id varchar(255) NOT NULL,
   status tinyint NOT NULL,
   PRIMARY KEY (friendship_id)
 );
 
 CREATE TABLE locations (
-  user_id int NOT NULL,
+  user_id varchar(255) NOT NULL,
   location_id int NOT NULL,
   PRIMARY KEY (user_id,location_id)
 );
 
 CREATE TABLE useraccounts (
-  user_id int NOT NULL AUTO_INCREMENT,
+  user_id varchar(255) NOT NULL,
   displayName varchar(45) NOT NULL,
   score int NOT NULL,
   leaderboardParticipant tinyint NOT NULL,
   difficulty varchar(45) NOT NULL,
-  PRIMARY KEY (user_id)
-);
+  PRIMARY KEY (`user_id`)
 
 ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'mysql';
 flush privileges;
