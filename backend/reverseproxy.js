3695878cef210036bb714b1ecb816087586e9cd6
akshatrajh <33471963+DentedGem@users.noreply.github.com>
2022-07-04 19:02:38 -0700

    replace single quote with double

--- backend/reverseproxy.js
+++ backend/reverseproxy.js
@@ -1,12 +1,12 @@
 // Source https://itnext.io/hosting-multiple-apps-on-the-same-server-implement-a-reverse-proxy-with-node-a4e213497345
 // Dependencies
-const express = require('express');
-const  proxy  = require('http-proxy-middleware');
+const express = require("express");
+const  proxy  = require("http-proxy-middleware");
 
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
 // Config
-const { routes } = require('./reverseproxyconfig.json');
+const { routes } = require("./reverseproxyconfig.json");
 
 const app = express();
 
@@ -15,12 +15,12 @@ for (route of routes) {
         createProxyMiddleware({
             target: route.address,
             pathRewrite: (path, req) => {
-                return path.split('/').slice(2).join('/'); // Could use replace, but take care of the leading '/'
+                return path.split("/").slice(2).join("/"); // Could use replace, but take care of the leading "/"
             }
         })
     );
 }
 
 app.listen(80, () => {
-    console.log('Proxy listening on port 80');
+    console.log("Proxy listening on port 80");
 });
