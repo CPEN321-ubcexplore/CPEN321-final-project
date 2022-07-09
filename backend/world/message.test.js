0d8b1131275e80569e7c4f40408c40bcba3442eb
akshatrajh <33471963+DentedGem@users.noreply.github.com>
2022-07-09 02:36:15 -0700

    i do not know how to test

--- backend/world/message.test.js
+++
@@ -1,8 +0,0 @@
-const Message = require('./world_server.js').Message;
-
-
-describe("Message tests", () => {
-    test('Delete all messages should return "All messages deleted"', () => {
-      expect(Message.deleteAllMessages()).toBe("All messages deleted");
-    });
-   })
