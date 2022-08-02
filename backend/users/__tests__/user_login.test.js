b732c9db388a5d38dc35a3d939e9c9e838324365
dpither <dylanpither@gmail.com>
2022-08-02 10:55:59 -0700

    Moved client secret to untracked keys.js in under backend/users

--- backend/users/__tests__/user_login.test.js
+++ backend/users/__tests__/user_login.test.js
@@ -6,19 +6,19 @@ const axios = require("axios");
 var test_token;
 const fake_token = "abc123";
 
+const { CLIENT_SECRET } = require("../keys");
 const CLIENT_ID = "239633515511-9g9p4kdqcvnnrnjq28uskbetjch6e2nc.apps.googleusercontent.com";
-const CLIENT_SECRET = "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";
 const REFRESH_TOKEN = "1//04KczeSbwVEinCgYIARAAGAQSNwF-L9Ir_Y7JjbuSChE5rHaXbnwPsDKJvpQ41htmlphXp1qd9LFg0HXcCDuHUzWPcfE83g47KmI";
 const refresh_token_url = "https://www.googleapis.com/oauth2/v4/token";
 const refresh_token_body = "client_id=" + CLIENT_ID + "&client_secret=" + CLIENT_SECRET + "&grant_type=refresh_token&refresh_token=" + REFRESH_TOKEN;
 
-beforeAll(async () => { 
+beforeAll(async () => {
     //Get test_token
-    try{
+    try {
         const refresh_res = await axios.post(refresh_token_url, refresh_token_body);
         console.log(refresh_res.data);
         test_token = refresh_res.data.id_token;
-    }catch(err){
+    } catch (err) {
         console.log(err);
     }
 
